a4dfda709f7ad15a579e7c6ecea8e58ddd53a076
Mannuru <arunm057@gmail.com>
2018-01-06 23:11:59 -0600

    Here is Reamme

---
+++ cp-tippy/SettingsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SettingsViewController.swift
+//  cp-tippy
+//
+//  Created by Arun Mannuru on 01/06/18.
+//  Copyright © 2018 Arun Mannuru. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var roundTipSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+        // MARK: - Navigation
+        // In a storyboard-based application, you will often want to do a little preparation before navigation override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func defaultTipChanged(_ sender: AnyObject) {
+        let tipIndex = tipControl.selectedSegmentIndex
+        
+        let defaults = UserDefaults.standard
+        defaults.set(tipIndex, forKey: "tipIndex")
+        defaults.set(true, forKey: "defaultChanged")
+        defaults.synchronize()
+        //print(String(tipIndex))
+    }
+    
+    @IBAction func roundTipSwitchChanged(_ sender: AnyObject) {
+        let isOn = roundTipSwitch.isOn
+        
+        let defaults = UserDefaults.standard
+        defaults.set(isOn, forKey: "roundTip")
+        defaults.synchronize()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = UserDefaults.standard
+        let intValue = defaults.integer(forKey: "tipIndex")
+        let roundTipBool = defaults.bool(forKey: "roundTip")
+        
+        tipControl.selectedSegmentIndex = intValue
+        roundTipSwitch.setOn(roundTipBool, animated: false)
+    }
+}
---
+++ cp-tippy/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  cp-tippy
+//
+//  Created by Arun Mannuru on 01/06/18.
+//  Copyright © 2018 Arun Mannuru. All rights reserved.
+//
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var paxStepper: UIStepper!
+    @IBOutlet weak var pricePerPersonLabel: UILabel!
+    @IBOutlet weak var numPersonLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        billField.becomeFirstResponder()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let bill = Double(billField.text!) ?? 0
+        if (bill == 0) {
+            billField.becomeFirstResponder()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages = [0.1, 0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        var tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let defaults = UserDefaults.standard
+        let roundTip = defaults.bool(forKey: "roundTip")
+        
+        if (roundTip) {
+            tip = ceil(tip)
+        }
+        
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        self.onPaxChanged(self)
+    }
+    
+    @IBAction func onPaxChanged(_ sender: AnyObject) {
+        let pax = paxStepper.value
+        
+        numPersonLabel.text = String(Int(pax))
+        
+        let total = totalLabel.text!
+        let index1 = total.index(total.startIndex, offsetBy: 1)
+        let price = Double(total.substring(from: index1))
+
+        let pricePerPerson = price! / pax
+        
+        pricePerPersonLabel.text = String(format: "Each person pays $%.2f", pricePerPerson)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = UserDefaults.standard
+        let bill = Double(billField.text!) ?? 0
+        
+        let defaultChanged = defaults.bool(forKey: "defaultChanged")
+        if (defaultChanged || bill == 0) { // only update segment if the default is changed
+            let intValue = defaults.integer(forKey: "tipIndex")
+            tipControl.selectedSegmentIndex = intValue
+            defaults.set(false, forKey: "defaultChanged") // updated
+        }
+        
+        self.calculateTip(self)
+    }
+}
+
